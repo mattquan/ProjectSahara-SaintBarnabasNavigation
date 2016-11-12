ed0215dadbaf1b2cd29b8c36c905ea6e9b5671ec
ywsang <ys2943@nyu.edu>
2016-11-11 22:41:33 -0500

    Added static variables for rooms and created room list

--- Keys.java
+++ Keys.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projectsahara;
+package ProjectSahara;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,6 +27,7 @@ public class Keys {
     final public static String nodeNameHallwayFloorOneD = "hallwayFloorOneD";
     final public static String nodeNameHallwayFloorOneE = "hallwayFloorOneE";
     final public static String nodeNameHallwayFloorOneF = "hallwayFloorOneF";
+    final public static String nodeNameHallwayFloorOneG = "hallwayFloorOneG";
     final public static String nodeNameElevatorFloorOne = "elevatorFloorOne";
     
     //floor two
@@ -70,19 +71,289 @@ public class Keys {
             "kids",
             "pediatric intensive care unit"
     ));
-    
-          
+         
+    //rooms
+    //floorOne
+    final public static String nodeNameRoom1323FloorOne = "room1323FloorOne";
+    final public static ArrayList<String> descriptionRoom1323FloorOne = new ArrayList<>(Arrays.asList(
+            "same day surgery",
+            "same day waiting area",
+            "same day",
+            "1323"
+    ));
+    final public static String nodeNameRoom1319FloorOne = "room1319FloorOne";
+    final public static ArrayList<String> descriptionRoom1319FloorOne = new ArrayList<>(Arrays.asList(
+            "mens bathroom",
+            "men's bathroom",
+            "bathroom",
+            "mens restroom",
+            "men's restroom",
+            "restroom"
+    ));
+    final public static String nodeNameRoom1318FloorOne = "room1318FloorOne";
+    final public static ArrayList<String> descriptionRoom1318FloorOne = new ArrayList<>(Arrays.asList(
+            "womens bathroom",
+            "womens's bathroom",
+            "bathroom",
+            "womens restroom",
+            "women's restroom",
+            "restroom"
+    ));
+    final public static String nodeNameRoomS12FloorOne = "roomS12FloorOne";
+    final public static ArrayList<String> descriptionRoomS12FloorOne = new ArrayList<>(Arrays.asList(
+            "same day surgery",
+            "same day holding",
+            "same day prep",
+            "same day recovery",
+            "same day",
+            "S12"     
+    ));
+    final public static String nodeNameRoomReceptionFloorOne = "roomReceptionFloorOne";
+    final public static ArrayList<String> descriptionRoomReceptionFloorOne = new ArrayList<>(Arrays.asList(
+            "reception",
+            "reception desk",
+            "information desk"
+    ));
+    final public static String nodeNameRoom1398FloorOne = "room1398FloorOne";
+    final public static ArrayList<String> descriptionRoom1398FloorOne = new ArrayList<>(Arrays.asList(
+            "chapel",
+            "1398"
+    ));
+    final public static String nodeNameRoom1211FloorOne = "room1211FloorOne";
+    final public static ArrayList<String> descriptionRoom1211FloorOne = new ArrayList<>(Arrays.asList(
+            "parent",
+            "parent lounge",
+            "1211"
+    ));
+    final public static String nodeNameRoom1214FloorOne = "room1214FloorOne";
+    final public static ArrayList<String> descriptionRoom1214FloorOne = new ArrayList<>(Arrays.asList(
+            "mens bathroom",
+            "men's bathroom",
+            "bathroom",
+            "mens restroom",
+            "men's restroom",
+            "restroom"
+    ));
+    final public static String nodeNameRoom1215FloorOne = "room1215FloorOne";
+    final public static ArrayList<String> descriptionRoom1215FloorOne = new ArrayList<>(Arrays.asList(
+            "womens bathroom",
+            "womens's bathroom",
+            "bathroom",
+            "womens restroom",
+            "women's restroom",
+            "restroom"
+    ));
+    final public static String nodeNameRoom1607FloorOne = "room1607FloorOne";
+    final public static ArrayList<String> descriptionRoom1607FloorOne = new ArrayList<>(Arrays.asList(
+            "ICU",
+            "ICU waiting",
+            "ICU waiting area",
+            "ICU waiting room",
+            "CCU",
+            "CCU waiting",
+            "CCU waiting area",
+            "CCU waiting room",
+            "1607"
+    ));
+    final public static String nodeNameRoom1730AFloorOne = "room1730AFloorOne";
+    final public static ArrayList<String> descriptionRoom1730AFloorOne = new ArrayList<>(Arrays.asList(
+            "radiation oncology"
+    ));
+    //floorTwo
+    final public static String nodeNameRoom2350AFloorTwo = "room2350AFloorTwo";
+    final public static ArrayList<String> descriptionRoom2350AFloorTwo = new ArrayList<>(Arrays.asList(
+            "phones",
+            "phone"
+    ));
+    final public static String nodeNameRoom2424FloorTwo = "room2424FloorTwo";
+    final public static ArrayList<String> descriptionRoom2424AFloorTwo = new ArrayList<>(Arrays.asList(
+            "desk",
+            "reception",
+            "information"
+    ));
+    //floorThree
+    final public static String nodeNameRoom3301DFloorThree = "room3301DFloorThree";
+    final public static ArrayList<String> descriptionRoom3301DFloorThree = new ArrayList<>(Arrays.asList(
+            "restroom",
+            "bathroom"
+    ));
+    final public static String nodeNameRoom3200C1FloorThree = "room3200C1FloorThree";
+    final public static ArrayList<String> descriptionRoom3200C1FloorThree = new ArrayList<>(Arrays.asList(
+            "desk",
+            "reception",
+            "information"
             
-    //rooms!
-    final public static ArrayList<Node> floorGroundRooms = new ArrayList<>(Arrays.asList(
-            new Node(Keys.nodeNameEastWingFloorGround,  new Edge[]{
-                    new Edge(324.2,Keys.nodeNameHallwayFloorGroundA,0),
-                    new Edge(369,Keys.nodeNameHallwayFloorGroundB,1)
-                })
+    ));
+    //floorFour
+    final public static String nodeNameRoom4235FloorFour = "room4235FloorFour";
+    final public static ArrayList<String> descriptionRoom4235FloorFour = new ArrayList<>(Arrays.asList(
+            "restroom",
+            "bathroom"
+    ));
+    final public static String nodeNameRoom4034FloorFour = "room4035FloorThree";
+    final public static ArrayList<String> descriptionRoom4034FloorFour = new ArrayList<>(Arrays.asList(
+            "desk",
+            "reception",
+            "information"
+            
+    ));
+    //ground floor rooms
+    final public static String nodeNameRoomG106FloorGround = "roomG106FloorGround";
+    final public static ArrayList<String> descriptionRoomG106FloorGround = new ArrayList<>(Arrays.asList(
+            "bathroom",
+            "restroom"
+    ));
+    final public static String nodeNameParkingValidationFloorGround = "parkingValidationFloorGround";
+    final public static ArrayList<String> descriptionParkingValidationFloorGround = new ArrayList<>(Arrays.asList(
+            "parking",
+            "validation",
+            "parking validation"
+    ));
+    final public static String nodeNameRoomG453FloorGround = "roomG453FloorGround";
+    final public static ArrayList<String> descriptionRoomG453FloorGround = new ArrayList<>(Arrays.asList(
+            "records",
+            "medical records",
+            "G453"
+    ));
+    final public static String nodeNameRoomG309BFloorGround = "roomG309BFloorGround";
+    final public static ArrayList<String> descriptionRoomG309BFloorGround = new ArrayList<>(Arrays.asList(
+            "outpatient lab",
+            "G309B"
+    ));
+    final public static String nodeNameRoomG336FloorGround = "roomG336FloorGround";
+    final public static ArrayList<String> descriptionRoomG336FloorGround = new ArrayList<>(Arrays.asList(
+            "clinical lab",
+            "G336"
+    ));
+    final public static String nodeNameRoomG230FloorGround = "roomG320FloorGround";
+    final public static ArrayList<String> descriptionRoomG320FloorGround = new ArrayList<>(Arrays.asList(
+            "cafeteria",
+            "food",
+            "cafe",
+            "G320"
+    ));
+    final public static String nodeNameRoomE00C6FloorGround = "roomE00C6FloorGround";
+    final public static ArrayList<String> descriptionRoomE00C6FloorGround = new ArrayList<>(Arrays.asList(
+            "burn",
+            "burn outpatient",
+            "E00C6"
+    ));final public static String nodeNameRoomE00C7FloorGround = "roomE00C7FloorGround";
+    final public static ArrayList<String> descriptionRoomE00C7FloorGround = new ArrayList<>(Arrays.asList(
+            "pediatric",
+            "pediatric outpatient",
+            "E00C7"
     ));
     
-   
     
+    //listOfRooms
+    final public static ArrayList<Node> listOfRooms = new ArrayList<>(Arrays.asList(
+            //floorOne
+            new Node(nodeNameRoom1323FloorOne, new Edge[]{
+                new Edge(3.020,nodeNameHallwayFloorOneA, 0),
+                new Edge(19.226,nodeNameHallwayFloorOneB,1),
+                new Edge(7.880,nodeNameElevatorFloorOne,2)
+            }),
+            new Node(nodeNameRoom1319FloorOne, new Edge[]{
+                new Edge(8.760,nodeNameHallwayFloorOneA, 0),
+                new Edge(12.830,nodeNameHallwayFloorOneB,1),
+                new Edge(1.832,nodeNameElevatorFloorOne,2)
+            }),
+            new Node(nodeNameRoom1318FloorOne, new Edge[]{
+                new Edge(11.753,nodeNameHallwayFloorOneA, 0),
+                new Edge(10.504,nodeNameHallwayFloorOneB,1),
+                new Edge(0.461,nodeNameElevatorFloorOne,2)
+            }),
+            new Node(nodeNameRoomS12FloorOne, new Edge[]{
+                new Edge(3.613,nodeNameHallwayFloorOneA, 0),
+                new Edge(19.166,nodeNameHallwayFloorOneB,1),
+                new Edge(1.615,nodeNameElevatorFloorOne,2)
+            }),
+            new Node(nodeNameRoomReceptionFloorOne, new Edge[]{
+                new Edge(5.403,nodeNameHallwayFloorOneA, 0),
+                new Edge(12.830,nodeNameHallwayFloorOneB,1),
+                new Edge(16.842,nodeNameElevatorFloorOne,2)
+            }),
+            new Node(nodeNameRoom1398FloorOne, new Edge[]{
+                new Edge(8.180,nodeNameHallwayFloorOneG, 0)
+            }),
+            new Node(nodeNameRoom1211FloorOne, new Edge[]{
+                new Edge(4.708,nodeNameHallwayFloorOneD, 0),
+                new Edge(5.991,nodeNameHallwayFloorOneE,1)
+            }),
+            new Node(nodeNameRoom1214FloorOne, new Edge[]{
+                new Edge(7.594,nodeNameHallwayFloorOneD, 0),
+                new Edge(3.254,nodeNameHallwayFloorOneE,1)
+            }),
+            new Node(nodeNameRoom1215FloorOne, new Edge[]{
+                new Edge(8.569,nodeNameHallwayFloorOneD, 0),
+                new Edge(2.190,nodeNameHallwayFloorOneE,1)
+            }),
+            new Node(nodeNameRoom1607FloorOne, new Edge[]{
+                new Edge(1.725,nodeNameHallwayFloorOneE,1)
+            }),
+            new Node(nodeNameRoom1730AFloorOne, new Edge[]{
+                new Edge(19.059,nodeNameHallwayFloorOneF, 0)
+            }),
+            //floorTwo
+            new Node(nodeNameRoom2350AFloorTwo, new Edge[]{
+                new Edge(9.679, nodeNameElevatorFloorTwo,0),
+                new Edge(9.581, nodeNameDeskFloorTwo, 1)    
+            }),
+            new Node(nodeNameRoom2424FloorTwo, new Edge[]{
+                new Edge(19.963, nodeNameElevatorFloorTwo,0),
+                new Edge(0, nodeNameDeskFloorTwo, 1)
+            }),
+            //floorThree
+            new Node(nodeNameRoom3200C1FloorThree, new Edge[]{
+                new Edge(27.445, nodeNameElevatorFloorThree,0),
+                new Edge(0, nodeNameDeskFloorThree, 1)
+            }),
+            new Node(nodeNameRoom3301DFloorThree, new Edge[]{
+                new Edge(5.543, nodeNameElevatorFloorThree,0),
+                new Edge(22.173, nodeNameDeskFloorThree,1)
+            }),
+            //floorFour
+            new Node(nodeNameRoom4235FloorFour, new Edge[]{
+                new Edge(12.719, nodeNameElevatorFloorFour,0),
+                new Edge(12.618, nodeNameDeskFloorFour, 1)
+            }),
+            new Node(nodeNameRoom4034FloorFour, new Edge[]{
+                new Edge(24.312, nodeNameElevatorFloorFour,0),
+                new Edge(0, nodeNameDeskFloorFour,1)
+            }),
+            //groundFloor
+            new Node(nodeNameRoomG106FloorGround, new Edge[]{
+                new Edge(109.818, nodeNameHallwayFloorGroundA, 0),
+                new Edge(30.576, nodeNameElevatorFloorGround, 1),
+                new Edge(47.168, nodeNameHallwayFloorGroundB, 2)
+            }),
+            new Node(nodeNameParkingValidationFloorGround, new Edge[]{
+                new Edge(44.291, nodeNameHallwayFloorGroundA, 0),
+                new Edge(40.912, nodeNameElevatorFloorGround, 1),
+                new Edge(112.693, nodeNameHallwayFloorGroundB, 2)
+            }),
+            new Node(nodeNameRoomG453FloorGround, new Edge[]{
+                new Edge(30.644, nodeNameHallwayFloorGroundC, 0)
+            }),
+            new Node(nodeNameRoomG309BFloorGround, new Edge[]{
+                new Edge(70.678, nodeNameHallwayFloorGroundC, 0)
+            }),
+            new Node(nodeNameRoomG336FloorGround, new Edge[]{
+                new Edge(238.684, nodeNameHallwayFloorGroundC, 0)
+            }),
+            new Node(nodeNameRoomG230FloorGround, new Edge[]{
+                new Edge(146.263, nodeNameHallwayFloorGroundA, 0),
+                new Edge(208.004, nodeNameEastWingFloorGround, 1)
+            }),
+            new Node(nodeNameRoomE00C7FloorGround, new Edge[]{
+                new Edge(266.703, nodeNameHallwayFloorGroundB, 0),
+                new Edge(99.181, nodeNameEastWingFloorGround, 1)
+            }),
+            new Node(nodeNameRoomE00C6FloorGround, new Edge[]{
+                new Edge(22.543, nodeNameHallwayFloorGroundB, 0),
+                new Edge(355.749, nodeNameEastWingFloorGround, 1)
+            })      
+    ));
             
             
     
