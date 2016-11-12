2eee790dd711ef829febcd3f1e7a19c6417859ab
ywsang <ys2943@nyu.edu>
2016-11-11 22:42:29 -0500

    Changed edge distance

--- FloorTwo.java
+++ FloorTwo.java
@@ -3,21 +3,20 @@ import java.util.*;
 
 //@author nickprimamore
 
-public class FloorTwo implements Graph{
+public class FloorTwo extends Graph{
 	
 	public ArrayList<Node> build(){
 		
-        
-
+      
 
         return new ArrayList<>(
                 //alphabetical order
             Arrays.asList(                    
                                    
                 new Node(Keys.nodeNameDeskFloorTwo, Keys.descriptionDeskFloorTwo, new Edge[]{
-                    new Edge(42,Keys.nodeNameElevatorFloorTwo,0)}),
+                    new Edge(19.963,Keys.nodeNameElevatorFloorTwo,0)}),
                 new Node(Keys.nodeNameElevatorFloorTwo, new Edge[]{
-                    new Edge(42,Keys.nodeNameDeskFloorTwo,0),})
+                    new Edge(19.963,Keys.nodeNameDeskFloorTwo,0),})
                 ));            
                
     }
